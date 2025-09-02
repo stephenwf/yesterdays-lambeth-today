d6ebcfe8da89a84b04646e2e0060b75ac804e639
Stephen Fraser <stephen.fraser@digirati.com>
2025-09-02 15:12:55 +0100

    Fixed workflow to use pnpm

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path';
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/yesterdays-lambeth-today/',
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        index: "./index.html",
+        index_vite: "./index-vite.html",
+        map: "./map.html",
+      },
+    }
+  }
+});
